809fdde2155a90c1973032c5e23bbe5ed697af46
Mmerichukwu Anosike <67147759+Roarnotes@users.noreply.github.com>
2022-05-15 22:19:25 +0100

    fixing bugs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,12 @@
 /** @format */
+// progress tracker
+let key1;
+let key2;
+let key3;
+let key4;
+let key5;
+let key6;
+let selectedOption;
 
 // hamburger btn
 const navbarcheckbox = document.getElementById("nav-id");
@@ -12,12 +20,14 @@ const eyeIcon2 = document.getElementById("eye2");
 // form vailditor
 const userName = document.getElementById("inputUsername4");
 const email = document.getElementById("inputEmail4");
+const gender = document.getElementById("inputGender4");
 const address = document.getElementById("inputAddress");
 const phoneNumber = document.getElementById("inputPhoneNumber4");
 const submitBtn = document.getElementById("btn");
 //error messages
 const usernameError = userName.nextElementSibling;
 const emailError = email.nextElementSibling;
+const genderError = gender.nextElementSibling;
 const addressError = address.nextElementSibling;
 const phoneNumberError = phoneNumber.nextElementSibling;
 const pass1 = passwordInputBox.nextElementSibling;
@@ -32,6 +42,7 @@ const warning3 = document.getElementById("warning3");
 const warning4 = document.getElementById("warning4");
 const warning5 = document.getElementById("warning5");
 const warning6 = document.getElementById("warning6");
+const warning7 = document.getElementById("warning7");
 
 function passwordDisplay(params) {
   if (passwordInputBox.type === "password" && passwordInputBox2.type === "password") {
@@ -67,6 +78,16 @@ function onSubmit() {
     addressCheck();
     passwordCheck();
     phoneNumberCheck();
+    if (key1 + key2 + key3 + key4 + key5 + key6 === 7) {
+        alert('THANK YOU VERY MUCH FOR JOINING');
+    } else {
+        // if (selectedOption === 1) {
+        //   genderSelect(1);
+        // } else {
+        //  noGender();
+        genderSelect(storeChoice());
+        alert('PLEASE FILL THE FORM PROPERLY');
+     }
 }
 
 function usernameCheck() {
@@ -74,11 +95,14 @@ function usernameCheck() {
 
   if (input === "") {
     usernameError.style.display = "inline-block";
-    warning1.classList.add("fa-circle-exclamation");
+      warning1.classList.add("fa-circle-exclamation");
+      key1 = -1;
   } else {
     usernameError.style.display = "none";
     warning1.classList.remove("fa-circle-exclamation");
-    warning1.classList.add("fa-circle-check");
+      warning1.classList.add("fa-circle-check");
+      key1 = 1;
+
   }
 }
 
@@ -89,11 +113,13 @@ function addressCheck() {
 
   if (input === "") {
     addressError.style.display = "inline-block";
-    warning3.classList.add("fa-circle-exclamation");
+      warning3.classList.add("fa-circle-exclamation");
+      key2 = -1;
   } else {
     addressError.style.display = "none";
     warning3.classList.remove("fa-circle-exclamation");
-    warning3.classList.add("fa-circle-check");
+      warning3.classList.add("fa-circle-check");
+      key2 = 1;
   }
 }
 
@@ -104,11 +130,13 @@ function phoneNumberCheck() {
 
   if (input === "") {
     phoneNumberError.style.display = "inline-block";
-    warning6.classList.add("fa-circle-exclamation");
+      warning6.classList.add("fa-circle-exclamation");
+      key3 = -1;
   } else {
     phoneNumberError.style.display = "none";
     warning6.classList.remove("fa-circle-exclamation");
-    warning6.classList.add("fa-circle-check");
+      warning6.classList.add("fa-circle-check");
+      key3 = 1;
   }
 }
 
@@ -124,14 +152,16 @@ function passwordCheck(params) {
     pass1.style.display = "none";
     pass2.style.display = "none";
     warning4.classList.add("fa-circle-exclamation");
-    warning5.classList.add("fa-circle-exclamation");
+      warning5.classList.add("fa-circle-exclamation");
+      key4 = -2;
   } else if (passwordSet === "" || passwordconfirm === "") {
     pass1.style.display = "inline-block";
     pass2.style.display = "inline-block";
     matchingPassWord1.style.display = "none";
     matchingPassWord2.style.display = "none";
     warning4.classList.add("fa-circle-exclamation");
-    warning5.classList.add("fa-circle-exclamation");
+      warning5.classList.add("fa-circle-exclamation");
+      key4 = -2;
   } else {
     pass1.style.display = "none";
     pass2.style.display = "none";
@@ -140,7 +170,8 @@ function passwordCheck(params) {
     warning4.classList.remove("fa-circle-exclamation");
     warning5.classList.remove("fa-circle-exclamation");
     warning4.classList.add("fa-circle-check");
-    warning5.classList.add("fa-circle-check");
+      warning5.classList.add("fa-circle-check");
+      key4 = 2;
   }
 }
 
@@ -158,12 +189,65 @@ const validate = () => {
   if (validateEmail(email)) {
     warning2.classList.add("fa-circle-check");
       warning2.classList.remove("fa-circle-exclamation");
-       emailError.style.display = "none";
+      emailError.style.display = "none";
+      key5 = 1;
   } else {
       warning2.classList.add("fa-circle-exclamation");
+      warning2.classList.remove('fa-circle-check');
       emailError.style.display = 'inline-block';
+      key5 = -1;
   }
   return false;
 };
 
-$("#inputEmail4").on("input", validate);
+$("#inputEmail4").on("input", validate); 
+
+function genderSelect(selected) {
+    // error logic
+    if (selected === undefined)
+    {
+        selected = 0;
+    }
+    else {
+     selectedOption = selected.value;   
+    }
+
+    if (selectedOption == 1) {
+       warning7.classList.add("fa-circle-check");
+        warning7.classList.remove("fa-circle-exclamation");
+        genderError.style.display = "none";
+       // selectedOption = 0;
+        key6 = 1;
+    }
+    else if (selectedOption == 2) {
+     warning7.classList.add("fa-circle-check");
+        warning7.classList.remove("fa-circle-exclamation");
+        genderError.style.display = "none";
+        // selectedOption = 0;
+        key6 = 1;
+    }
+    else {
+        noGender();
+        key6 = -1;
+    }
+
+    storeChoice(selectedOption);
+
+}
+
+function noGender(params) {
+    warning7.classList.remove("fa-circle-check");
+    warning7.classList.add("fa-circle-exclamation");
+    genderError.style.display = "inline-block";
+    key6 = -1;
+}
+
+const storeChoice = (value) => {
+    
+    if (value === undefined) {
+        value = 0;
+        return value;
+    } else {
+        return value;
+    }
+};
